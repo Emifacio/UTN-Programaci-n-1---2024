499de2ba90fb997b5df6847329adf24e21822734
Gabriel Emiliano Facio <127351922+Emifacio@users.noreply.github.com>
2024-09-14 13:40:24 -0300

    Initial commit

---
+++ Ejercicios/ejemplo01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+int num1, num2, resultado;
+ cout << "Ingresar un nro entero: ";
+ cin >> num1;
+ cout << "Ingresar otro nro entero: ";
+ cin >> num2;
+
+ // Asi se alinean sentencias en una sola linea.Con el mismo comando "cout"
+ resultado = num1 + num2;
+ cout << endl << endl << "El resultado de la suma es: " << resultado;
+
+
+
+  return 0;
+}
